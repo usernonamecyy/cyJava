c718b6316f9484cbeb566ec2aa0ec5657c581c29
cyy <924226627@qq.com>
2023-09-20 09:04:55 +0800

    9.19

--- src/MyTestFirst.java
+++ src/MyTestFirst.java
@@ -1,2 +1,36 @@
+//学习Java的第一天
+
+/**
+ * 在需要注释的位置，输入“/**”，然后按一下enter即可实现
+ */
 public class MyTestFirst {
-}
+    public String name;
+    public String food;
+    public int age;
+
+    public void hisAge(){
+        name="Baby";
+        age=20;
+        System.out.println("He is "+name +" and he is "+age);
+
+    }
+    public static void hisLike(){
+        String food;
+        food = "kfc";
+        System.out.println(food);
+    }
+
+    public static void main(String[] args) {
+        /** 要先构造方法并实例化*/
+        String hisName;
+        MyTestFirst testFirst = new  MyTestFirst();
+        hisName = testFirst.name;
+        System.out.println(hisName);
+        testFirst.hisAge();
+
+        System.out.println("===============");
+        hisLike();
+
+            }
+        }
+
