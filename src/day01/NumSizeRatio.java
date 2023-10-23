8258a2cda2131f027f58969b2223c3c65fb12e62
cyy <924226627@qq.com>
2023-10-23 17:05:55 +0800

    update

--- src/day01/NumSizeRatio.java
+++ src/day01/NumSizeRatio.java
@@ -16,21 +16,33 @@ public class NumSizeRatio {
 
         if(num1>num2){
             if (num2>num3){
-                System.out.println(num3+num2+num1);
+                System.out.println(num3+"<"+num2+"<"+num1);
             }
             //num2<num3
             else if (num1>num3){
-                System.out.println(num1+num2+num3);
+                System.out.println(num2+"<"+num3+"<"+num1);
             }
             //num1<num3
             else {
-                System.out.println(num3+num2+num1);
+                System.out.println(num2+"<"+num1+"<"+num3);
             }
             }
         else if (num2>num3){
-
+            //1<2
+            if(num1>num3){
+                System.out.println(num3+"<"+num1+"<"+num2);
             }
+            else {
+                //1<3
+                System.out.println(num1+"<"+num3+"<"+num2);
+            }
+
+            }else {
+            System.out.println(num1+"<"+num2+"<"+num3);
         }
 
+        }
+
+
     }
-}
+
