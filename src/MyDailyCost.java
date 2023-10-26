066950dd449f043614daf7c0596ab1881b2f1fe9
cyy <924226627@qq.com>
2023-10-26 18:08:41 +0800

    想计算

---
+++ src/MyDailyCost.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class MyDailyCost {
+    public BigDecimal sumcost;
+    public BigDecimal dailycost;
+    
+    public static void main(String[] args) {
+        Scanner daysum = new Scanner(System.in);
+    }
+}
