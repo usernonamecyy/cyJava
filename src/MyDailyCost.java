251baefb97bc710655ac65bcb2d3982c1fd84860
cyy <924226627@qq.com>
2023-10-27 11:14:03 +0800

    尚未想到解决50 100的方法

--- src/MyDailyCost.java
+++ src/MyDailyCost.java
@@ -2,10 +2,57 @@ import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class MyDailyCost {
-    public BigDecimal sumcost;
-    public BigDecimal dailycost;
-    
+    //先打折再-2
+
+
     public static void main(String[] args) {
-        Scanner daysum = new Scanner(System.in);
+        int daysum ;
+        int day = 0;
+        BigDecimal buscost = new BigDecimal("1");
+        BigDecimal subcost = new BigDecimal("6");
+        BigDecimal sumgocost = new BigDecimal("0");
+        BigDecimal sumbackcost = new BigDecimal("0");
+        BigDecimal dailycost = new BigDecimal("0");
+        BigDecimal allpay = new BigDecimal("0");
+
+        for (daysum=24;day<daysum;day++){
+            if (allpay.compareTo(new BigDecimal("50")) < 0){
+                BigDecimal sum1 = buscost.multiply(new BigDecimal("0.9"));
+                BigDecimal sum2 = subcost.multiply(new BigDecimal("0.9"));
+                BigDecimal sum4 = subcost.multiply(new BigDecimal("0.9"));
+                sumgocost = sum1.add(sum2).subtract(new BigDecimal("2"));
+                allpay  = allpay.add(sumgocost).add(sum4);
+
+                System.out.println("sumgocost:"+""+sumgocost);
+                System.out.println("backcost"+""+sum4);
+                System.out.println("allpay"+""+allpay);
+
+            }
+            else if (allpay.compareTo(new BigDecimal("50")) > 0 && allpay.compareTo(new BigDecimal("100")) < 0)
+            {
+                BigDecimal sum1 = buscost.multiply(new BigDecimal("0.7"));
+                BigDecimal sum2 = subcost.multiply(new BigDecimal("0.7"));
+                BigDecimal sum4 = subcost.multiply(new BigDecimal("0.7"));
+                sumgocost = sum1.add(sum2).subtract(new BigDecimal("2"));
+                allpay  = allpay.add(sumgocost).add(sum4);
+
+                System.out.println("sumgocost:"+""+sumgocost);
+                System.out.println("backcost"+""+sum4);
+                System.out.println("allpay"+""+allpay);
+            }
+            else {
+                BigDecimal sum1 = buscost.multiply(new BigDecimal("0.5"));
+                BigDecimal sum2 = subcost.multiply(new BigDecimal("0.5"));
+                BigDecimal sum4 = subcost.multiply(new BigDecimal("0.5"));
+                sumgocost = sum1.add(sum2).subtract(new BigDecimal("2"));
+                allpay  = allpay.add(sumgocost).add(sum4);
+
+                System.out.println("sumgocost:"+""+sumgocost);
+                System.out.println("backcost"+""+sum4);
+                System.out.println("allpay"+""+allpay);
+            }
+        }
+
+
     }
 }
