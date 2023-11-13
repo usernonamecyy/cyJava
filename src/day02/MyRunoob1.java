ec447f1978007a8219a08c26c6bd915cb21563a9
Yuan <924226627@qq.com>
2023-11-13 14:51:12 +0800

    1

---
+++ src/day02/MyRunoob1.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class MyRunoob1 {
+        public MyRunoob1(String name)
+    {
+        System.out.println("name is"+name);
+    }
+
+    public static void main(String[] args) {
+        MyRunoob1 myPuppy = new MyRunoob1("jack");
+    }
+
+}
