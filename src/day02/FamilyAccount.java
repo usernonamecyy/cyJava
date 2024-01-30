5862a9dea806d200a4c45b3b91209dbc03a66eca
Yuan <924226627@qq.com>
2024-01-30 16:48:11 +0800

    B站 宋 43节

---
+++ src/day02/FamilyAccount.java
@@ -0,0 +1,20 @@
+package day02;
+
+class FamilyAccount {
+    public static void main(String[] args) {
+        boolean isFlag = true;
+        while(isFlag){
+            System.out.println("-----------家庭收支记账软件-----------\n");
+            System.out.println("1、收支明细");
+            System.out.println("2、登记收入");
+            System.out.println("3、登记支出");
+            System.out.println("4、退出\n");
+            System.out.println("请选择1-4: ");
+
+
+
+
+//            isFlag = false;
+        }
+    }
+}
---
+++ src/day02/VariableTest.java
@@ -0,0 +1,28 @@
+package day02;
+
+public class VariableTest {
+    public static void main(String[] args) {
+
+        byte b1 = 12;
+        byte b2 = -128;
+        short s1 = 128;
+        int i1 = 1234;
+        long l1 = 1000L;
+        float f1 = 1.72F;
+        double d1 = 3.1415;
+        char c1 = 'C';//只能写一个字符
+        System.out.println(l1);
+        System.out.println(d1);
+        System.out.println(d1+1);
+        System.out.println(c1);
+        boolean isMarried = true;
+        if (isMarried){
+            System.out.println("886");
+        }
+
+
+
+    }
+
+
+}
