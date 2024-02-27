f880bae8b99aa1f842d85e182e8d9a1db6e862d6
Yuan <924226627@qq.com>
2024-02-27 16:59:35 +0800

    B站 宋 83节

---
+++ src/day05/RecursionTest.java
@@ -0,0 +1,34 @@
+package day05;
+
+public class RecursionTest {
+    public static void main(String[] args) {
+        RecursionTest test = new RecursionTest();
+        //test.method1();
+        System.out.println(test.getSum(100));
+    }
+
+    /**
+     * 如下递归方法的调用，会导致StackOverflowError
+
+    public  void  method1(){
+        System.out.println("method1");
+        method1();
+    }
+}
+     */
+    public  int getSum(int num){
+    int sum = 0;
+    for(int i = 0;i<=num;i++){
+        sum+=i;
+    }
+    return sum;
+    }
+    public  int getSum1(int num){
+        if (num==1){
+            return 1;
+        }
+        else {
+            return num+getSum(num-1);
+        }
+    }
+}
